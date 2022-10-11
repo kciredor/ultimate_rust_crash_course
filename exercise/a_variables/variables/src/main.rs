91b0b6fdbb12a983b8952b14fc88e2bbf5737b9e
Roderick Schaefer <roderick@wehandle.it>
2022-10-11 09:16:46 +0200

    kciredor: Exercise A

---
+++ exercise/a_variables/variables/src/main.rs
@@ -0,0 +1,21 @@
+const STARTING_MISSILES: i32 = 8;
+const READY_AMOUNT: i32 = 2;
+
+fn main() {
+    // let mut missiles = STARTING_MISSILES;
+    // let ready: i32 = READY_AMOUNT;
+
+    // let (mut missiles, ready): (i32, i32) = (STARTING_MISSILES, READY_AMOUNT);
+
+    let (missiles, ready): (i32, i32) = (STARTING_MISSILES, READY_AMOUNT);
+
+    let notinuse = 0;
+    READY_AMOUNT = 3;
+
+    println!("Firing {} of {} missiles", ready, missiles);
+
+    // missiles = missiles - ready;
+    // println!("{} missiles left", missiles);
+
+    println!("{} missiles left", missiles - ready);
+}
